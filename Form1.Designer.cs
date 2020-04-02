1a168b6b736d829c045e59ce82b01dff36a99ce4
Renato Azevedo <renatoads1@gmail.com>
2020-04-02 19:21:55 -0300

    primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace pegaxymouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_coordenadas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_coordenadas
+            // 
+            this.lbl_coordenadas.AutoSize = true;
+            this.lbl_coordenadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_coordenadas.Location = new System.Drawing.Point(12, 19);
+            this.lbl_coordenadas.Name = "lbl_coordenadas";
+            this.lbl_coordenadas.Size = new System.Drawing.Size(142, 26);
+            this.lbl_coordenadas.TabIndex = 1;
+            this.lbl_coordenadas.Text = "Coordenadas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_coordenadas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_coordenadas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pegaxymouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            lbl_coordenadas.Text = "x = " + e.X + " y = " + e.Y;
+        }
+
+        private void Form1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            lbl_coordenadas.Text = "x = " + e.X + " y = " + e.Y;
+        }
+    }
+}
